e4cac520b559846f9ea643e8c9b6c9d0ec27a9d0
gogyu <gogyu@DESKTOP-KF14CO4>
2021-08-23 17:55:29 +0900

    52강 상단메뉴처리 및 로그아웃

--- src/kr/co/softcampus/interceptor/TopMenuInterceptor.java
+++ src/kr/co/softcampus/interceptor/TopMenuInterceptor.java
@@ -1,14 +1,17 @@
 package kr.co.softcampus.interceptor;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import kr.co.softcampus.beans.BoardInfoBean;
+import kr.co.softcampus.beans.UserBean;
 import kr.co.softcampus.service.TopMenuService;
 
 public class TopMenuInterceptor implements HandlerInterceptor {
@@ -17,11 +20,16 @@ public class TopMenuInterceptor implements HandlerInterceptor {
 	@Autowired
 	private TopMenuService topMenuService;
 	
+	@Resource(name="loginUserBean")
+	@Lazy
+	private UserBean loginUserBean;
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		List<BoardInfoBean> topMenuList=topMenuService.getTopMenuList();
 		request.setAttribute("topMenuList", topMenuList);
+		request.setAttribute("loginUserBean", loginUserBean);
 		return true;
 	}
 }
