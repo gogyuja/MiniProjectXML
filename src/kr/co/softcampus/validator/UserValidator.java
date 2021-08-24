4e9ccef1e5521ee77891bc8135a6b17ccc68f1e6
gogyu <gogyu@DESKTOP-KF14CO4>
2021-08-25 01:31:46 +0900

    54강 정보 수정 처리

--- src/kr/co/softcampus/validator/UserValidator.java
+++ src/kr/co/softcampus/validator/UserValidator.java
@@ -16,22 +16,27 @@ public class UserValidator implements Validator {
 	@Override
 	public void validate(Object target, Errors errors) {
 		// TODO Auto-generated method stub
-		UserBean userBean = (UserBean) target;
-		// validate 검사를 할때 사용되는 빈을 확인한다.
-		// 회원가입시에는 joinUserBean 로그인시에는 tempLoginUserBean이 사용된다
-		// 이를 식별하는 이유는 로그인시에는 DontCheckUserIdExist를 검사하지 말아야 하기 때문이다.
-		String beanName = errors.getObjectName();
-
-		if (beanName.equals("joinUserBean")) {
+		UserBean userBean=(UserBean)target;
+		
+		//validate 검사를 할때 사용되는 빈을 확인한다.
+		//회원가입시에는 joinUserBean 로그인시에는 tempLoginUserBean이 사용된다
+		//이를 식별하는 이유는 로그인시에는 DontCheckUserIdExist를 검사하지 말아야 하기 때문이다.
+		String beanName=errors.getObjectName();
+		
+		if (beanName.equals("joinUserBean") || beanName.equals("modifyUserBean")) {
 			if (userBean.getUser_pw().equals(userBean.getUser_pw2()) == false) {
 				errors.rejectValue("user_pw", "NotEquals");
 				errors.rejectValue("user_pw2", "NotEquals");
 			}
-
-			if (userBean.isUserIdExist() == false) {
-				errors.rejectValue("user_id", "DontCheckUserIdExist");
-			}
 		}
+		
+		//회원가입을 할때 동일회원존재여부를 체크하지 않았을 때를 확인
+		if(beanName.equals("joinUserBean")){
+				if (userBean.isUserIdExist() == false) {
+					errors.rejectValue("user_id", "DontCheckUserIdExist");
+				}
+		}
+		
 	}
 
 }
