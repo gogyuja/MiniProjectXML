1812ce4aef5d4c537130256b76b161280fa7ac5d
gogyu <gogyu@DESKTOP-KF14CO4>
2021-09-02 17:34:50 +0900

    55강 글작성하기1

--- src/kr/co/softcampus/controller/BoardController.java
+++ src/kr/co/softcampus/controller/BoardController.java
@@ -1,15 +1,25 @@
 package kr.co.softcampus.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.co.softcampus.beans.ContentBean;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 	
 	@GetMapping("/main")
-	public String main() {
+	public String main(@RequestParam("board_info_idx")int board_info_idx, Model model) {
+		model.addAttribute("board_info_idx",board_info_idx);
 		return "board/main";
 	}
 	
@@ -19,8 +29,16 @@ public class BoardController {
 	}
 	
 	@GetMapping("/write")
-	public String write() {
+	public String write(@ModelAttribute("writeContentBean")ContentBean writeCointentBean) {
 		return "board/write";
+	}
+	
+	@PostMapping("/write_pro")
+	public String write_pro(@Valid @ModelAttribute("writeContentBean") ContentBean writeContentBean,BindingResult result) {
+		if(result.hasErrors()) {
+			return "board/write";
+		}
+		return "board/write_success";
 	}
 	
 	@GetMapping("/modify")
